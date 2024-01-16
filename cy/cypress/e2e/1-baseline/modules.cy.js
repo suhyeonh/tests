cdb6d7f70f5fa5748cc65cc5c757bd40514d8083
jackwrfuller <jack.fuller101@gmail.com>
2024-01-17 10:56:08 +1100

    Fix modules version check

--- cy/cypress/e2e/1-baseline/modules.cy.js
+++ cy/cypress/e2e/1-baseline/modules.cy.js
@@ -1,14 +1,15 @@
 import packages from '../../../../../composer.json'
 
 let modules = Object.entries(packages['require']).filter((str) => {
-    return str[1].indexOf('^') === -1
+  return str[1].indexOf('^') === 0
 })
 
+
 describe('Cross check module and library versions', () => {
-    modules.forEach((module, i) => {
-        it(`${module}`, () => {
-            const moduleVersion = module[1].substring(0, module[1].indexOf(" as"));
-            cy.composerCommand('show ' + module[0] + ' | grep versions').its('stdout').should('contain', moduleVersion)
-        })
+  modules.forEach((module, i) => {
+    it(`${module}`, () => {
+      const moduleVersion = module[1].substring(0, module[1].indexOf(" as"));
+      cy.composerCommand('show ' + module[0] + ' | grep versions').its('stdout').should('contain', moduleVersion)
     })
+  })
 })
