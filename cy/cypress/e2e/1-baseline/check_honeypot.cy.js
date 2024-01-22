ed0570fc765cf7b2657dc606ab7f9087c733e1c7
jackwrfuller <jack.fuller101@gmail.com>
2024-01-23 09:25:31 +1100

    add password reset honeypot check

--- cy/cypress/e2e/1-baseline/check_honeypot.cy.js
+++ cy/cypress/e2e/1-baseline/check_honeypot.cy.js
@@ -1,4 +1,30 @@
+import {randString} from "../../support/commands";
+
+const randomString = randString(10).toLowerCase()
+
 describe('Check Honeypot traps are enabled on webforms', () => {
 
+
+  it('Check Honeypot module is enabled', () => {
+    cy.execDrush('pm:list | grep honeypot | grep Enabled')
+    cy.execDrush(`cset -y honeypot.settings element_name ${randomString}`)
+  })
+
+  it('Check Honeypot is present on password reset form', () => {
+    cy.visit('user/password')
+    cy.get(`[data-drupal-selector="edit-${randomString}"]`)
+  })
+
+  it('Check Honeypot completion prevents password reset submission', () => {
+    cy.visit('user/password')
+    cy.get('[data-drupal-selector="edit-name"]').type('test@example.com')
+    cy.get(`[data-drupal-selector="edit-${randomString}"]`).type('Im a hacker!', {force: true})
+    // Wipe Watchdog logs
+    cy.execDrush('wd all -y')
+    // Submit form
+    cy.confirm()
+    // Check Watchdog logs for honeypot
+    cy.execDrush('ws | grep honeypot')
+  })
 
 })
