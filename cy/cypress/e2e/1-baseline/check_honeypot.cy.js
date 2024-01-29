36300a77d2da84f714a7fdf3afb728efa1be274b
jwrf <jack.fuller101@gmail.com>
2024-01-29 16:44:49 +1100

    fix honeypot test

--- cy/cypress/e2e/1-baseline/check_honeypot.cy.js
+++ cy/cypress/e2e/1-baseline/check_honeypot.cy.js
@@ -1,6 +1,7 @@
 import {randString} from "../../support/commands";
 
-const randomString = randString(10).toLowerCase()
+// const randomString = randString(10).toLowerCase()
+const randomString = "jackfuller";
 
 describe('Check Honeypot traps are enabled on webforms', () => {
 
@@ -8,6 +9,7 @@ describe('Check Honeypot traps are enabled on webforms', () => {
   it('Check Honeypot module is enabled', () => {
     cy.execDrush('pm:list | grep honeypot | grep Enabled')
     cy.execDrush(`cset -y honeypot.settings element_name ${randomString}`)
+    cy.execDrush(`cset -y honeypot.settings protect_all_forms true`)
   })
 
   it('Check Honeypot is present on password reset form', () => {
@@ -16,17 +18,19 @@ describe('Check Honeypot traps are enabled on webforms', () => {
   })
 
   it('Check Honeypot completion prevents password reset submission', () => {
+    // Enabled logging to ensure honeypot blocking can be checked
+    cy.execDrush('pm:install dblog -y')
+
     cy.visit('user/password')
     cy.get('[data-drupal-selector="edit-name"]').type('test@example.com')
     cy.get(`[data-drupal-selector="edit-${randomString}"]`).type('Im a hacker!', {force: true})
-    // Enabled logging to ensure honeypot blocking can be checked
-    cy.execDrush('pm:install dblog -y')
+
     // Wipe Watchdog logs
     cy.execDrush('wd all -y')
     // Submit form
     cy.confirm()
     // Check Watchdog logs for honeypot
-    cy.execDrush('ws | grep honeypot')
+    cy.execDrush('ws | grep "Blocked user attempting to reset password."')
     //disabled Watchdog
     cy.execDrush('pm:uninstall dblog -y')
   })
