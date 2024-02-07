f727215d70d784bb106f805882a2ec51f1c6486d
Joseph Zhao <pandaski@outlook.com.au>
2024-02-07 15:41:40 +1100

    Update module_versions.cy.js

--- cy/cypress/e2e/3-modules/module_versions.cy.js
+++ cy/cypress/e2e/3-modules/module_versions.cy.js
@@ -1,15 +1,10 @@
 import packages from '../../../../../composer.json'
 
 let modules = Object.entries(packages['require']).filter((str) => {
-    return str[1].indexOf('^') === 0
+    return str[1].indexOf('^') === -1
 })
 
-
 describe('Cross check module and library versions', () => {
-    it('Ensure modules are installed', () => {
-        cy.composerCommand('install')
-    })
-
     modules.forEach((module, i) => {
         it(`${module}`, () => {
             const moduleVersion = module[1].substring(0, module[1].indexOf(" as"));
