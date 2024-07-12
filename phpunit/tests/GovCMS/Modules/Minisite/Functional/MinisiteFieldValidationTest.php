f4807e844c89c050681909694c6b57d27c7edf7e
Joseph Zhao <pandaski@outlook.com.au>
2024-07-12 16:12:30 +1000

    The minisite module tests are good now

--- phpunit/tests/GovCMS/Modules/Minisite/Functional/MinisiteFieldValidationTest.php
+++ phpunit/tests/GovCMS/Modules/Minisite/Functional/MinisiteFieldValidationTest.php
@@ -25,17 +25,6 @@ class MinisiteFieldValidationTest extends BaseFieldValidationTest {
   #[\Override]
   protected function setUp(): void {
     parent::setUp();
-
-    // Get the module handler.
-    $module_handler = \Drupal::service('module_handler');
-    // Get the version of the minisite module.
-    $module_info = $module_handler->getModule('minisite');
-    $version = $module_info->info['version'] ?? '0.0.0';
-
-    // Check if the version is less than 2.2.0.
-    if (version_compare($version, '2.2.0', '<')) {
-      $this->markTestSkipped('Minisite module version is less than 2.2.0.');
-    }
   }
 
 }
