35a4bfa273a666e71ad1fde2088f5b868c2aee01
suhyeonh <suhyeonh@gmail.com>
2024-07-30 13:08:11 +1000

    update test

---
+++ phpunit/integration/GovCMS/Modules/event_log_track/Functional/EventsLogTrackTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace GovCMS\Tests\Integration\GovCMS\Modules\event_log_track\Functional;
+
+use Drupal\Tests\event_log_track\Functional\EventsLogTrackTestBase as BaseEventsLogTrackTest;
+
+/**
+ * Test class for Event log track test functionality with GovCMS profile.
+ *
+ * @group govcms
+ * @group eventlogtrack
+ */
+class EventsLogTrackTestBase extends BaseEventsLogTrackTest {
+
+    // Use the GovCMS profile
+    protected $profile = 'govcms';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+    }
+}
